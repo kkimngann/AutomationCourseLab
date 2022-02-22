8bdf51365f16cf98f56a29990026753726499ed6
ngan.nguyen16 <ngan.nguyen16@tiki.vn>
2022-02-22 22:41:22 +0700

    Update rounding number and remove redundant init value

--- src/lesson_02/lab_2_1and2_3.java
+++ src/lesson_02/lab_2_1and2_3.java
@@ -1,13 +1,15 @@
 package lesson_02;
 
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class lab_2_1and2_3 {
 
     public static void main(String[] args) {
-        float bmi = 0;
-        float weightGap = 0;
+        float bmi;
+        float weightGap;
+        DecimalFormat decimalFormat = new DecimalFormat("0.0");
         System.out.print("Please in your weight(kg): ");
         Scanner scanner = new Scanner(System.in);
         float weight = scanner.nextFloat();
@@ -20,15 +22,15 @@ public class lab_2_1and2_3 {
         bmi = weight / (height  * 2);
         if(bmi <=18.5){
             weightGap = 18.5f * (height * 2) - weight;
-            System.out.printf("You are under weight and your need to increase %f(kg) to get normal weight\n",weightGap);
+            System.out.printf("You are under weight and your need to increase %s (kg) to get normal weight\n",decimalFormat.format(weightGap));
         } else if( bmi <=24.9){
             System.out.printf("You are normal weight and your bmi is %f\n",bmi);
         } else {
             weightGap = weight- 18.5f * (height * 2);
             if(bmi <=29.9){
-                System.out.printf("You are over weight and your need to decrease %f (kg) to get normal weight\n",weightGap);
+                System.out.printf("You are over weight and your need to decrease %s (kg) to get normal weight\n",decimalFormat.format(weightGap));
             }else {
-                System.out.printf("You are obesity and your need to decrease %f(kg) to get normal weight\n", weightGap);
+                System.out.printf("You are obesity and your need to decrease %s (kg) to get normal weight\n", decimalFormat.format(weightGap));
             }
         }
     }

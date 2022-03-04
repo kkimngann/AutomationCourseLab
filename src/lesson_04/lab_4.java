fa289061eeac9331370526862012f328ce595f1c
ngan.nguyen16 <ngan.nguyen16@tiki.vn>
2022-03-04 22:12:04 +0700

    syntax

--- src/lesson_04/lab_4.java
+++ src/lesson_04/lab_4.java
@@ -24,61 +24,59 @@ public class lab_4 {
                 maxValue = value;
             }
         }
+        System.out.println("List option:\n" +
+                "1. Add number to array list\n" +
+                "2.Print list numbers\n" +
+                "3. Find max number\n" +
+                "4. Find min number\n" +
+                "5. Find number\n" +
+                "0. Exit");
 
-            System.out.println("List option:\n" +
-                    "1. Add number to array list\n" +
-                    "2.Print list numbers\n" +
-                    "3. Find max number\n" +
-                    "4. Find min number\n" +
-                    "5. Find number\n" +
-                    "0. Exit");
-
-            Scanner scanner = new Scanner(System.in);
-            boolean isContinue = true;
-            while (isContinue) {
-                System.out.println("\n=======================\nPlease select an option:");
-                int mySelected = scanner.nextInt();
-                switch (mySelected) {
-                    case 0:
-                        isContinue = false;
+        Scanner scanner = new Scanner(System.in);
+        boolean isContinue = true;
+        while (isContinue) {
+            System.out.println("\n=======================\nPlease select an option:");
+            int mySelected = scanner.nextInt();
+            switch (mySelected) {
+                case 0:
+                    isContinue = false;
+                    break;
+                case 1:
+                    System.out.println("Please input your number:");
+                    int myNumber = scanner.nextInt();
+                    myList = addNumber(myList, myNumber);
+                    break;
+                case 2:
+                    System.out.println("This is your list:");
+                    for (int i : myList) {
+                        System.out.printf("%d,",i);
+                    }
+                    break;
+                case 3:
+                    System.out.printf("Your max number is: %d\n", findMax(myList));
+                    break;
+                case 4:
+                    System.out.printf("Your min number is: %d\n", findMin(myList));
+                    break;
+                case 5:
+                    System.out.println("Please input your number:");
+                    int findNumber = scanner.nextInt();
+                    ArrayList<Integer> myListIndex  = findNumber(myList, findNumber);
+                    if(myListIndex.size() == 0){
+                        System.out.println("Do not existing your number in list");
                         break;
-                    case 1:
-                        System.out.println("Please input your number:");
-                        int myNumber = scanner.nextInt();
-                        myList = addNumber(myList, myNumber);
-                        break;
-                    case 2:
-                        System.out.println("This is your list:");
-                        for (int i : myList) {
-                            System.out.printf("%d,",i);
-                        }
-                        break;
-                    case 3:
-                        System.out.printf("Your max number is: %d\n", findMax(myList));
-                        break;
-                    case 4:
-                        System.out.printf("Your min number is: %d\n", findMin(myList));
-                        break;
-                    case 5:
-                        System.out.println("Please input your number:");
-                        int findNumber = scanner.nextInt();
-                        ArrayList<Integer> myListIndex  = findNumber(myList, findNumber);
-                        if(myListIndex.size() == 0){
-                            System.out.println("Do not existing your number in list");
-                            break;
-                        }
-                        System.out.println("This is your list index:");
-                        for (int i : myListIndex) {
-                            System.out.printf("%d,",i);
-                        }
-                        break;
-                    default:
-                        System.out.println("Please input a value in menu");
-                        break;
-                }
-
+                    }
+                    System.out.println("This is your list index:");
+                    for (int i : myListIndex) {
+                        System.out.printf("%d,",i);
+                    }
+                    break;
+                default:
+                    System.out.println("Please input a value in menu");
+                    break;
             }
         }
+    }
 
     private static ArrayList<Integer> addNumber(ArrayList<Integer> myList, int myNumber) {
         myList.add(myNumber);
@@ -114,5 +112,4 @@ public class lab_4 {
         }
         return maxValue;
     }
-
 }
